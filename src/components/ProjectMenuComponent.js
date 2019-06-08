58392597cf6ead510a059aaf47abc24f2e17fd87
mayoko <mayokoex@gmail.com>
2019-06-08 16:38:01 +0900

    add Modal to ProjectPage Component, it is necessary to move this modal to another new file

--- src/components/ProjectMenuComponent.js
+++ src/components/ProjectMenuComponent.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Menu, Item } from 'react-contexify';
 import 'react-contexify/dist/ReactContexify.min.css';
 
-const ProjectMenuComponent = (props) => (
-    <Menu id={props.id}>
-        <Item onClick={() => {console.log("click!");}}>new project</Item>
-        <Item onClick={() => {console.log("click!");}}>delete this project</Item>
-    </Menu>
-);
+class ProjectMenuComponent extends React.Component {
+    render() {
+        return (
+            <Menu id={this.props.id}>
+                <Item onClick={() => {console.log("click!");}}>new project</Item>
+                <Item onClick={() => {console.log("click!");}}>delete this project</Item>
+            </Menu>
+        )
+    }
+}
 
 export default ProjectMenuComponent;
--- src/components/ProjectPageComponent.js
+++ src/components/ProjectPageComponent.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ProjectComponent from './ProjectComponent';
 import ProjectDescriptionComponent from './ProjectDescriptionComponent';
+import Modal from 'react-modal';
+
+const customStyles = {
+    content : {
+        top        : '0%',
+        left       : '50%',
+        right      : 'auto',
+        bottom     : 'auto',
+        marginright: '-50%',
+        transform  : 'translate(-50%, 50%'
+    }
+};
+Modal.setAppElement('body');
 
 class ProjectPageComponent extends React.Component {
     constructor(props) {
@@ -8,12 +21,53 @@ class ProjectPageComponent extends React.Component {
         this.root_project_id = "0";
         this.handleProjectDescription = this.handleProjectDescription.bind(this);
         this.state = {
-            project_id : "0"
+            project_id : "0",
+            modalIsOn  : false,
+            // modal
+            modalIsOpen : false
         };
+
+        // modal
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
+
+    openModal() {
+        this.setState({modalIsOpen : true});
+    }
+
+    afterOpenModal() {
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen : false});
+    }
+
     render() {
         return (
             <div class="project">
+                <button onClick={this.openModal}>Open Modal</button>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+                    <h2>Add Project</h2>
+                    <form>
+                        <p><label>
+                            project name : <input type="text" name="project_name" value={this.state.project_name_text} onChange={(e) => this.setState({project_name_text : e.target.value})} />
+                        </label></p>
+                        <p>project detail :</p>
+                        <p><label>
+                            <textarea name="project_detail" rows="4" cols="40" value={this.state.project_detail_text} onChange={(e) => this.setState({project_detail_text : e.target.value})} />
+                        </label></p>
+                        <button>add project</button>
+                    </form>
+                    <button onClick={this.closeModal}>close</button>
+                </Modal>
                 <ProjectComponent
                     project_id={this.root_project_id}
                     handleProjectDescription={this.handleProjectDescription}
